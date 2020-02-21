cefb31ee8287b8d16561ec57dee9912481076ab7
Lera <khil.mut.hsp@gmail.com>
2020-02-21 14:27:31 +0200

    Setting the home page by role

---
+++ ScheduleApp/Views/Home/TeacherHome.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ScheduleApp.Views.Home
+{
+    public class TeacherHomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
